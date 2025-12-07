4cdf1684c0620431f0c9b7e05f73a5dea20b193a
00simba <sameer15557@gmail.com>
2025-12-06 20:39:38 -0500

    updated aws env key server

--- server/server.js
+++ server/server.js
@@ -18,8 +18,8 @@ const upload = multer({ storage: multer.memoryStorage() });
 const AWS = require("aws-sdk");
 
 AWS.config.update({
-  accessKeyId: process.env.MY_AWS_ACCESS_KEY,
-  secretAccessKey: process.env.MY_AWS_SECRET_KEY,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   region: "us-east-1",
 });
 
