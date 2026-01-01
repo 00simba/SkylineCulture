bbb7a4a3cccf1d2e68bad9db3d7b551bdeaa8714
00simba <sameer15557@gmail.com>
2026-01-01 18:31:08 -0500

    updated genereateProductJsonLd.ts

--- skylineculture/lib/generateProductJsonLd.ts
+++ skylineculture/lib/generateProductJsonLd.ts
@@ -23,14 +23,6 @@ export function generateProductJsonLd(product : Product, url: string){
       availability: "https://schema.org/InStock",
       itemCondition: "https://schema.org/NewCondition",
     },
-
-    aggregateRating: product.stars
-      ? {
-          "@type": "AggregateRating",
-          ratingValue: product.stars,
-          reviewCount: Math.max(product.stars * 5, 1),
-        }
-      : undefined,
   };
   return json;
 }
